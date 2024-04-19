dd56cbb5f11e50871abde954e1abec715380f688
Deepak Sharma <d33paksharma.39@gmail.com>
2024-04-19 12:23:15 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -16,11 +16,9 @@ connection(); // Establish database connection
 app.use(express.json({ limit: '100mb' }));
 
 // Set up CORS headers
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With,credentials, Content-Type, Accept");
-  next();
-});
+app.use(cors({
+  origin: 'http://jsram.aifuturevision.in'
+}));
 
 app.get("/",(req,res)=>{
   res.send("Welcome to homepage")
